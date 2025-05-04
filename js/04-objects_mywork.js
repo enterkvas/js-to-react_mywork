0f14717dc28db7515078e3bc7ef9b760303b9a1f
enterkvas <alexanderbugakov66@gmail.com>
2025-05-03 23:24:37 -0700

    5: Part 2: did Exercise 1.3.

--- js/04-objects_mywork.js
+++ js/04-objects_mywork.js
@@ -4,7 +4,7 @@
 //
 // -------------------------------------------------------------
 
-// 1.1. (5:08) Destructuring:
+// 1.1. (5:08) Destructuring на основе использования свойств(ключей) объекта в качестве переменных, передаваемых в виде параметров (в фигрурных скобках) в функцию-компонент:
 
 // function Checkbox({id, label, defaultChecked}){
 //     return `
@@ -23,35 +23,45 @@
 //     defaultChecked: false
 // });
 
-// function Checkbox({id, label, defaultChecked}){
+// -------------------------------------------------
+
+// 1.2. (14:40) Destructuring с использованием оператора rest:
+
+// function Avatar({src, alt: name, ...rest}){
 //     return `
-//     <label for='${id}'>${label}</label>
-//     <input id='${id}' type='checkbox' ${defaultChecked ? 'checked' : ''}>
+//     <img src="${src}" alt="${name}" style="border-radius: ${rest.borderRadius}; width: 200px;" />    
 //     `;
 // }
 
-// const app = document.getElementById('app');
+// const appDiv = document.getElementById('app');
 
-// app.innerHTML = Checkbox({
-//     id: 'checkbox',
-//     label: 'Subscribe for update',
-//     defaultChecked: false 
-// })
+// appDiv.innerHTML = Avatar({
+//     src: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+//     alt: 'Bob Jones',
+//     borderRadius: '10px'
+// });
 
-// 1.2. (14:40) Destructuring:
+// -------------------------------------------------
 
-function Avatar({src, alt: name, ...rest}){
+// 1.3. (19:04) Destructuring с использованием параметров "по умолчанию" (суть: если в объекте отсутвует какое-либо (или все сразу) из описанных свойств, то всё равно будут на странице выводиться значения "по умолчанию", переданные в параметрах объекта):
+
+function Message({text="No message", author='Someone', time=''}){
     return `
-    <img src="${src}" alt="${name}" style="border-radius: ${rest.borderRadius}" width="200px" />    
+    <header class="message">
+        <strong>${author}</strong> commented ${time}
+    </header>
+    <p>${text}</p>
     `;
 }
 
 const appDiv = document.getElementById('app');
 
-appDiv.innerHTML = Avatar({
-    src: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    alt: 'Bob Jones',
-    borderRadius: '10px'
-})
+appDiv.innerHTML = Message({
+    text: 'Called client, they reassured me the invoice would be paid by the 25th.',
+    author: 'Chelsea Hagon',
+    time: '3d ago'
+});
 
-// 1.3. (19:04) Destructuring:
+// -------------------------------------------------
+
+// 1.4. (23:33) 
