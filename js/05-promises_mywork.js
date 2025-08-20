64ca219b22729d53035c45c43e9e37bb3607b6c1
enterkvas <alexanderbugakov66@gmail.com>
2025-08-19 19:41:18 -0700

    18: Part 2 (repeat): file: 05-promises_mywork.js: all tasks completed. Starting to take Bogdan's Course

--- js/05-promises_mywork.js
+++ js/05-promises_mywork.js
@@ -62,34 +62,34 @@
 
 // Variant 1: 
 
-const appDiv = document.getElementById('app');
+// const appDiv = document.getElementById('app');
 
-function getText(){
-    fetch('https://jsonplaceholder.typicode.com/todos/10')
-        .then(response => {
-            if(!response.ok){
-                throw new Error('Unsuccessfully response')
-            }
-            return response.json();
-        })
-        .then(json => {
-            console.log(json);
-            document.querySelector('#text').textContent = json.title;
-        })
-        .catch(err => {
-            console.log('Error fetching data: ', err);
-            document.querySelector('p').textContent = 'Error fetching data';
-        }).finally(() => {
-            document.querySelector('#loading').remove()
-        })        
-}
+// function getText(){
+//     fetch('https://jsonplaceholder.typicode.com/todos/10')
+//         .then(response => {
+//             if(!response.ok){
+//                 throw new Error('Unsuccessfully response')
+//             }
+//             return response.json();
+//         })
+//         .then(json => {
+//             console.log(json);
+//             document.querySelector('#text').textContent = json.title;
+//         })
+//         .catch(err => {
+//             console.log('Error fetching data: ', err);
+//             document.querySelector('p').textContent = 'Error fetching data';
+//         }).finally(() => {
+//             document.querySelector('#loading').remove()
+//         })        
+// }
 
-getText()
+// getText()
 
-appDiv.innerHTML = `
-    <span id="loading">Loading...</span>
-    <p>*This text <strong id="text"></strong> other any text.</p>
-`;
+// appDiv.innerHTML = `
+//     <span id="loading">Loading...</span>
+//     <p>*This text <strong id="text"></strong> other any text.</p>
+// `;
 
 //-------------------------------------------
 
@@ -124,7 +124,6 @@ appDiv.innerHTML = `
 //     <p>* There are <strong id="text">--</strong> other parts. </p>
 // `;
 
-
 //==================================================
 
 // 5.4. (1:16:10) (async/await):
@@ -132,7 +131,7 @@ appDiv.innerHTML = `
 // const appDiv = document.getElementById('app');
 
 // async function getText() {    
-//     const response = await fetch('https://jsonplaceholder.typicode.com/todos/10');
+//     const response = await fetch('https://jsonplaceholder.typicode.com/todos/10')
 //     const data = await response.json();
 //     console.log(data);
 //     document.querySelector('#toText').textContent = data.title;
@@ -148,26 +147,26 @@ appDiv.innerHTML = `
 
 // 5.5. (1:23:05) (async/await с catch и finally):
 
-// const appDiv = document.getElementById('app');
+const appDiv = document.getElementById('app');
 
-// async function getText() {
-//         try {
-//                 const response = await fetch('https://jsonplaceholder.typicode.com/todos/10');
-//                 if(!response.ok) throw new Error('Error making request');                
-//                 const data = await response.json();
-//                 console.log(data);
-//                 document.querySelector('#text').textContent = data.title;
-//         } catch (error){
-//                 console.log(error);
-//                 document.querySelector('p').textContent = 'Unsuccessfully response';
-//         } finally {
-//                 document.querySelector('#loading').remove(); 
-//         }
-// }
+async function getText() {
+        try {
+                const response = await fetch('https://jsonplaceholder.typicode.com/todos/10');
+                if(!response.ok) throw new Error('Error making request');                
+                const data = await response.json();
+                console.log(data);
+                document.querySelector('#text').textContent = data.title;
+        } catch (error){
+                console.log(error);
+                document.querySelector('p').textContent = error;
+        } finally {
+                document.querySelector('#loading').remove(); 
+        }
+}
 
-// getText()
+getText()
 
-// appDiv.innerHTML = `
-//         <span id='loading'>Loading...</span>
-//         <p>The text: <strong id='text'>--</strong> other code.</p>
-// `;
+appDiv.innerHTML = `
+        <span id='loading'>Loading...</span>
+        <p>The text: <strong id='text'>--</strong> other code.</p>
+`;
